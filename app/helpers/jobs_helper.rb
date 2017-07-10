00f611f17e7bffae6b9d8b7ff57d2a2ec02efb43
Akira Suzuki <akiradeves@gmail.com>
2017-07-11 03:01:54 +0900

    invite friends not login

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,11 +2,14 @@ module JobsHelper
   
   def is_applied(job)
     is_applied = false
-    job.proposals.each do |proposal|
-      if proposal.user_id == current_user.id
-        is_applied = true
+    if current_user.present?
+      job.proposals.each do |proposal|
+        if proposal.user_id == current_user.id
+          is_applied = true
+        end
       end
     end
+    
 
     return is_applied
 
